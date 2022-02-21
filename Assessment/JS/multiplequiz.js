d8679f4a1f180345f72598ab49c5a483b86bd411
Kyle Duncan <40530314@live.napier.ac.uk>
2022-02-21 09:21:56 +0000

    Website upload

---
+++ Assessment/JS/multiplequiz.js
@@ -0,0 +1,215 @@
+<<<<<<< HEAD
+
+function check(){ 
+	var score=0;
+	var audio1 = new Audio("Sound/failed.mp3")
+	var q1=document.Quiz.question1.value;
+	var q2=document.Quiz.question2.value;
+	var q3=document.Quiz.question3.value;
+	var q4=document.Quiz.question4.value;
+	var q5=document.Quiz.question5.value;
+	var q6=document.Quiz.question6.value;
+	var q7=document.Quiz.question7.value;
+	var q8=document.Quiz.question8.value;
+	var q9=document.Quiz.question9.value;
+	var q10=document.Quiz.question10.value;
+	//Fill in the blank one
+	var answer1 = box1.value;
+	var answer2 = box2.value;
+	var answer3 = box3.value;
+	var answer4 = box4.value;
+	var answer5 = box5.value;
+	var answer6 = box6.value;
+	//Fill in the blank two
+	var answer7 = box7.value;
+	var answer8 = box8.value;
+	var answer9 = box9.value;
+	var answer10 = box10.value;
+	var answer11 = box11.value;
+	var answer12 = box12.value;
+	var answer13 = box13.value;
+	//Fill in the blank three
+	var answer14 = box14.value;
+	var answer15 = box15.value;
+	var answer16 = box16.value;
+	var answer17 = box17.value;
+	var answer18 = box18.value;
+	var answer19 = box19.value;
+	//True or False 
+	var tf1=document.Quiz.question11.value;
+	var tf2=document.Quiz.question12.value;
+	var tf3=document.Quiz.question13.value;
+	var tf4=document.Quiz.question14.value;
+	var tf5=document.Quiz.question15.value;
+	var tf6=document.Quiz.question16.value;
+	var tf7=document.Quiz.question17.value;
+	var tf8=document.Quiz.question18.value;
+	var tf9=document.Quiz.question19.value;
+	var tf10=document.Quiz.question20.value;
+	
+	//multiple choice answers
+	if (q1=="Dr Jekyll and Mr Hyde and Frankensteins monster") {score++}
+	if (q2=="Anakin Skywalker") {score++}
+	if (q3=="Playstation 2") {score++}
+	if (q4=="Phil Jackson") {score++}
+	if (q5=="The Joker") {score++}
+	if (q6=="Han Solo") {score++}
+	if (q7=="Sonic the Hedgehog") {score++}
+	if (q8=="Samuel Eto'o") {score++}
+	if (q9=="Kryptonite") {score++}
+	if (q10=="Darth Sidious") {score++}
+	//Fill in the blank set one 
+	if (answer1 == "Darth Plagueis" || answer1 == "darth plagueis") {score++}
+	if (answer2 == "Jedi" || answer2 =="jedi") {score++}
+	if (answer3 == "Dark Lord" || answer3 == "dark lord") {score++}
+	if (answer4 == "Sith" || answer4 == "sith") {score++}
+	if (answer5 == "Powerful" || answer5 == "powerful") {score++}
+	if (answer6 == "Wise" || answer6 =="wise") {score++}
+	// Fill in the blank set two
+	if (answer7 == "War" || answer7 =="war") {score++}
+	if (answer8 == "Kratos" || answer8 == "kratos") {score++}
+	if (answer9 == "Ares" || answer9 == "ares") {score++}
+	if (answer10 == "War" || answer10 ==" war") {score++}
+	if (answer11 == "Zeus" || answer11 == "zeus") {score++}
+	if (answer12 == "Underworld" || answer12 =="underworld") {score++}
+	if (answer13 == "Sea" || answer13 == "sea") {score++}
+	//Fill in the blank set three
+	if (answer14 == "Clark Kent" || answer14 =="clark kent") {score++}
+	if (answer15 == "Bruce Wayne" || answer15 == "bruce wayne") {score++}
+	if (answer16 == "Diana Prince" || answer16 == "diana prince") {score++}
+	if (answer17 == "Steve Rodgers" || answer17 == "steve rodgers") {score++}
+	if (answer18 == "Peter Parker" || answer18 == "peter parker") {score++}
+	if (answer19 == "Natasha Romanov" || answer19 == "natasha romanov") {score++}
+	//True or false Answers
+	if (tf1=="True") {score++}
+	if (tf2=="True") {score++}
+	if (tf3=="False") {score++}
+	if (tf4=="False") {score++}
+	if (tf5=="True") {score++}
+	if (tf6=="True") {score++}
+	if (tf7=="False") {score++}
+	if (tf8=="True") {score++}
+	if (tf9=="True") {score++}
+	if (tf10=="True") {score++}
+
+
+	alert(name + " score was " + score +"/39");
+	
+	if (score>=30) {score="Welldone, however the quiz is over now"}
+	if (score >= 11 && score <=29) {score="You know you can do better"}
+	if (score<=10) {score="Use Google next time"}
+			audio1.play();
+	alert(score);
+    
+	}
+
+	
+
+=======
+
+function check(){ 
+	var score=0;
+	var audio1 = new Audio("Sound/failed.mp3")
+	var q1=document.Quiz.question1.value;
+	var q2=document.Quiz.question2.value;
+	var q3=document.Quiz.question3.value;
+	var q4=document.Quiz.question4.value;
+	var q5=document.Quiz.question5.value;
+	var q6=document.Quiz.question6.value;
+	var q7=document.Quiz.question7.value;
+	var q8=document.Quiz.question8.value;
+	var q9=document.Quiz.question9.value;
+	var q10=document.Quiz.question10.value;
+	//Fill in the blank one
+	var answer1 = box1.value;
+	var answer2 = box2.value;
+	var answer3 = box3.value;
+	var answer4 = box4.value;
+	var answer5 = box5.value;
+	var answer6 = box6.value;
+	//Fill in the blank two
+	var answer7 = box7.value;
+	var answer8 = box8.value;
+	var answer9 = box9.value;
+	var answer10 = box10.value;
+	var answer11 = box11.value;
+	var answer12 = box12.value;
+	var answer13 = box13.value;
+	//Fill in the blank three
+	var answer14 = box14.value;
+	var answer15 = box15.value;
+	var answer16 = box16.value;
+	var answer17 = box17.value;
+	var answer18 = box18.value;
+	var answer19 = box19.value;
+	//True or False 
+	var tf1=document.Quiz.question11.value;
+	var tf2=document.Quiz.question12.value;
+	var tf3=document.Quiz.question13.value;
+	var tf4=document.Quiz.question14.value;
+	var tf5=document.Quiz.question15.value;
+	var tf6=document.Quiz.question16.value;
+	var tf7=document.Quiz.question17.value;
+	var tf8=document.Quiz.question18.value;
+	var tf9=document.Quiz.question19.value;
+	var tf10=document.Quiz.question20.value;
+	
+	//multiple choice questions
+	if (q1=="Dr Jekyll and Mr Hyde and Frankensteins monster") {score++}
+	if (q2=="Anakin Skywalker") {score++}
+	if (q3=="Playstation 2") {score++}
+	if (q4=="Phil Jackson") {score++}
+	if (q5=="The Joker") {score++}
+	if (q6=="Han Solo") {score++}
+	if (q7=="Sonic the Hedgehog") {score++}
+	if (q8=="Samuel Eto'o") {score++}
+	if (q9=="Kryptonite") {score++}
+	if (q10=="Darth Sidious") {score++}
+	//Fill in the blank set one
+	if (answer1 == "Darth Plagueis" || answer1 == "darth plagueis") {score++}
+	if (answer2 == "Jedi" || answer2 =="jedi") {score++}
+	if (answer3 == "Dark Lord" || answer3 == "dark lord") {score++}
+	if (answer4 == "Sith" || answer4 == "sith") {score++}
+	if (answer5 == "Powerful" || answer5 == "powerful") {score++}
+	if (answer6 == "Wise" || answer6 =="wise") {score++}
+	// Fill in the blank set two
+	if (answer7 == "War" || answer7 =="war") {score++}
+	if (answer8 == "Kratos" || answer8 == "kratos") {score++}
+	if (answer9 == "Ares" || answer9 == "ares") {score++}
+	if (answer10 == "War" || answer10 ==" war") {score++}
+	if (answer11 == "Zeus" || answer11 == "zeus") {score++}
+	if (answer12 == "Underworld" || answer12 =="underworld") {score++}
+	if (answer13 == "Sea" || answer13 == "sea") {score++}
+	//Fill in the blank set three
+	if (answer14 == "Clark Kent" || answer14 =="clark kent") {score++}
+	if (answer15 == "Bruce Wayne" || answer15 == "bruce wayne") {score++}
+	if (answer16 == "Diana Prince" || answer16 == "diana prince") {score++}
+	if (answer17 == "Steve Rodgers" || answer17 == "steve rodgers") {score++}
+	if (answer18 == "Peter Parker" || answer18 == "peter parker") {score++}
+	if (answer19 == "Natasha Romanov" || answer19 == "natasha romanov") {score++}
+	//True or false Answers
+	if (tf1=="True") {score++}
+	if (tf2=="True") {score++}
+	if (tf3=="False") {score++}
+	if (tf4=="False") {score++}
+	if (tf5=="True") {score++}
+	if (tf6=="True") {score++}
+	if (tf7=="False") {score++}
+	if (tf8=="True") {score++}
+	if (tf9=="True") {score++}
+	if (tf10=="True") {score++}
+
+
+	alert(name + " score was " + score +"/39");
+	
+	if (score>=30) {score="Welldone, however the quiz is over now"}
+	if (score >= 11 && score <=29) {score="You know you can do better"}
+	if (score<=10) {score="Use Google next time"}
+			audio1.play();
+	alert(score);
+    
+	}
+
+	
+
+>>>>>>> 82acdced31b1235a27f32208852241b6a1835f4c
